ea049bc0fc0fbae442c274e540d8f732392195b0
Ignacio_Garcia <iggarsaudev@gmail.com>
2025-06-24 18:48:27 +0200

    merge con Lucia

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom";
 import "./privateLayout.css";
+import useAuth from "../hooks/useAuth";
 
 export default function PrivateLayout() {
+  const { logout } = useAuth();
+
   return (
     <div className="private-layout">
       <header className="private-layout__header">
         <h1 className="private-layout__title">Area Privada</h1>
+
+        <div className="header__actions">
+          <button onClick={logout} className="header__login-btn">
+            Logout
+          </button>
+        </div>
       </header>
       <main className="private-layout__main">
         <Outlet />
