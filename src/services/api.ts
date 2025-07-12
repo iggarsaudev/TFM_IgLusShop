2fc11677841d6d3a682fb6fb680d229d9cf0ac13
Ignacio García Sausor <iggarsaudev@gmail.com>
2025-07-12 19:31:28 +0200

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // ajusta si es necesario
+  //baseURL: "http://localhost:8000", // ajusta si es necesario
+  baseURL: "https://iglusshop.rf.gd/api"
   withCredentials: false, // no usamos cookies ahora, sino token
 });
 
